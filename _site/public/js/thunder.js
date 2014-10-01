23a229c6978834c9bc8bab4028d7b690fa8d94a2
David Strong <dstrong@locationinsight.com>
2014-09-30 22:44:25 -0600

    photo gallery

--- _site/public/js/thunder.js
+++ _site/public/js/thunder.js
@@ -3,6 +3,11 @@
 	$(document).ready(function() {
 		$('.carousel a.carousel-control').on('click', function() {
 			$(this).blur();
+		});
+		$('.image-item img').on('click', function() {
+			var image = $(this).attr('src');
+			$('#galleryModal img').attr('src', image);
+			$('#galleryModal').modal('show');
 		})
 	});
 })();
